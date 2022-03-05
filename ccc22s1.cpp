aedf33d5cc2f8dec89a75915e8ce2c59cbf9500d
PavithranChelliahpillai <100388060+PavithranChelliahpillai@users.noreply.github.com>
2022-03-05 08:42:03 -0500

    Add files via upload

---
+++ ccc22s1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+const int MAX=1e9;
+typedef tree<pair<int, int> , null_type, less<pair<int, int>>, rb_tree_tag,tree_order_statistics_node_update> oset; oset bst;
+#define int long long int
+#define endl '\n'
+#define fendl endl
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define INF 0x3f3f3f3f
+//#pragma GCC optimize("O3","unroll-loops")
+//#pragma GCC optimize("trapv")
+//#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+int32_t main() {
+    fio;
+    int a; cin>>a; int dp[MAX]; 
+    if (a==0) {
+        cout<<0<<endl; return 0;
+    }
+    dp[0]=1;
+    for (int i=0; i<a; i++) {
+
+    }
+}
