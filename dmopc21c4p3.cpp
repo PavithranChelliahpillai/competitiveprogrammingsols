935dba6fd9066c6c584e653470e92e5fe44e19ec
Pavithran Chelliahpillai <100388060+PavithranChelliahpillai@users.noreply.github.com>
2022-03-05 22:46:48 -0500

    Add files via upload

---
+++ dmopc21c4p3.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout<<"0"<<endl<<"10001"<<endl<<"10101"<<endl<<"11011"<<endl<<"11111"<<endl<<"10201"<<endl<<"12021"<<endl<<"11211"<<endl<<"10301"<<endl<<"13031"<<endl;
+}
