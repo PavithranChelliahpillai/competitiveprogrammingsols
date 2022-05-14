31057740a8b3458610b82085f6f403ccd8dfd979
Pavithran Chelliahpillai <100388060+PavithranChelliahpillai@users.noreply.github.com>
2022-05-14 09:46:28 -0400

    Add files via upload

--- dmopc19c3p4.cpp
+++ dmopc19c3p4.cpp
@@ -3,13 +3,15 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 using namespace __gnu_pbds;
-const int MAX=1e9;
+const int MAX=1e6;
+const int MOD=1e9+7;
 typedef tree<pair<int, int> , null_type, less<pair<int, int>>, rb_tree_tag,tree_order_statistics_node_update> oset; oset bst;
 #define int long long int
+#define pb push_back
 #define endl '\n'
 #define fendl endl
 #define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-#define INF 0x3f3f3f3f
+#define INF 0x3f3f3f3f3f3f3f3f
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 char _;
 //#pragma GCC optimize("O3","unroll-loops")
@@ -18,32 +20,34 @@ char _;
 int max(int a, int b) {return (a>b)?a:b;}
 int min(int a, int b) {return (a<b)?a:b;}
 int ceil (int x, int y) {return (x+y-1)/y;}
-gp_hash_table<int,int>dsu, freq;
-//if too slow, use vector instead
-int f(int a) {
-    if (dsu.find(a)==dsu.end()) dsu[a]=a, freq[a]=1;
-    if (dsu[a]!=a) dsu[a]=f(dsu[a]); 
-    return dsu[a];
-}
-void u(int a, int b) {
-    if (dsu.find(a)==dsu.end()) dsu[a]=a, freq[a]=1;
-    if (dsu.find(b)==dsu.end()) dsu[b]=b, freq[b]=1;
-    if (dsu[f(a)]!=dsu[f(b)]) {
-        freq[f(a)]+=freq[f(b)];
-        dsu[f(b)]=f(a);
+vector<int>adj[MAX]; int dp[MAX], ans, clr[MAX];
+void dfs (int v, int p, int tot) {
+    int v1=0, v2=0; 
+    for (auto u: adj[v]) {
+        if (u==p) continue;
+        dfs(u, v, tot); 
+        v2+=(v1+v2)*dp[u];
+        v1+=dp[u]; 
+        v1%=MOD, v2%=MOD;
+    }
+    if (clr[v]==tot) {
+        dp[v]=(1+v1+v2)%MOD;
+        ans=(ans+dp[v])%MOD;
+    }
+    else {
+        dp[v]=(v1+v2)%MOD;
+        ans=(ans+v2)%MOD;
     }
 }
-int n, m;
+int n; string s;
 int32_t main() {
     fio;
-    cin>>n>>m;
-    while (m--) {
-        int k; cin>>k; vector<int>li;
-        for (int i=0, a; i<k; i++) cin>>a, li.push_back(a);
-        for (int i=1; i<li.size(); i++) u(li[i], li[i-1]);
+    cin>>n>>s;
+    for (int i=1; i<=n; i++) clr[i]=s[i-1]=='R';
+    for (int i=1, u, v; i<n; i++) {
+        cin>>u>>v;
+        adj[u].pb(v), adj[v].pb(u);
     }
-    vector<int>sol;
-    for (int i=1; i<=n; i++) if (f(1)==f(i)) sol.push_back(i);
-    cout<<sol.size()<<endl;
-    for (auto i:sol) cout<<i<<" ";
+    dfs(1,0,1); dfs(1,0,0);
+    cout<<ans<<endl;
 }
